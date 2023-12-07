4d7848c631736151321cf083ca6f1830e7e2105a
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2023-12-07 21:50:13 +0000

    install

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp -r $HOME/Nvim/nvim $HOME/.config
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
